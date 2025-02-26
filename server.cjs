c9e0779555fe82584b27e22bdfd096b8284f11f7
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-02-26 09:44:33 -0700

    installed express

--- server.cjs
+++ server.cjs
@@ -1,1 +1,11 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+
+app.listen(process.env.PORT, ( )=> {
+
+    console.log('listening on port ${process.env.PORT}');
+
+});
