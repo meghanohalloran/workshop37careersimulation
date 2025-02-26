45304d0d880ee894e987a2efc25389c395f8cbf9
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-02-26 09:25:10 -0700

    bcrypt added

--- db/users.cjs
+++ db/users.cjs
@@ -1,6 +1,9 @@
 const client = require ('./client.cjs');
+const bcrypt = require('bcrypt');
 const createUser = async (userName,password) => {
   try {
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    console.log (encryptedPassword);
     await client.query(`
       INSERT INTO users (name, password)
       VALUES ('${userName}','${password}');
